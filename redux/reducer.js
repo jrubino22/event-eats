3b8a0978d14d6182f8be2f7b50bf49107edbdedd
Joseph Rubino <joeyrubi22@gmail.com>
2022-03-11 15:49:18 -0800

    begin redux setup

---
+++ redux/reducer.js
@@ -0,0 +1,9 @@
+import { MENU_ITEMS } from "../shared/menu-items";
+
+export const initialState = {
+    menuItems: MENU_ITEMS
+}
+
+export const Reducer = (state = initialState, action) => {
+    return state;
+}

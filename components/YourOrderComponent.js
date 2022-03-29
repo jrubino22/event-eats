5dcb6a1dcadc8b57b384bdffea61207cbd3cd286
Joseph Rubino <joeyrubi22@gmail.com>
2022-03-29 13:18:39 -0700

    more Your Order

--- components/YourOrderComponent.js
+++ components/YourOrderComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Text, FlatList, View, TouchableOpacity } from 'react-native'
+import { Text, FlatList, View, TouchableOpacity } from 'react-native';
+import {ListItem} from 'react-native-elements';
 import { connect } from 'react-redux';
 
 const mapStateToProps = state => {
@@ -25,13 +26,24 @@ class YourOrder extends Component {
 
     render() {
         const {navigate} = this.props.navigation
+        const renderYourOrder = ({item}) => {
+            return(
+                <View>
+                    <ListItem
+                        title={item.name}
+                        subtitle={this.props.yourOrder.option1}
+                        onPress={() => navigate('OrderForm', {menuId: item.id})}
+                    />
+                </View>
+            )
+        }
         return (
             <View animation='fadeInRightBig' duration={2000}>
                 <FlatList
                     data={this.props.menuItems.filter(
-                        menuItems => this.props.yourOrder.menuItemId.includes(menuItems.id)
+                        menuItems => this.props.yourOrder.includes(menuItems.id)
                     )}
-                    renderItem={renderFavoriteItem}
+                    renderItem={renderYourOrder}
                     keyExtractor={item => item.id.toString()}
                 />
             </View>
